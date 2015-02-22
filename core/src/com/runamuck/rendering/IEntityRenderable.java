70b2048c868eedaa3e53918e391786515f537ed3
Colin Juillard <colin.juillard@gmail.com>
2015-02-22 11:35:39 -0600

    Missed a couple files on last commit.

---
+++ core/src/com/runamuck/rendering/IEntityRenderable.java
@@ -0,0 +1,7 @@
+package com.runamuck.rendering;
+
+import com.runamuck.simulation.Entity;
+
+public interface IEntityRenderable extends IRenderable {
+	Entity getEntity();
+}
---
+++ core/src/com/runamuck/rendering/SpriteEntityRenderable.java
@@ -0,0 +1,54 @@
+package com.runamuck.rendering;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.runamuck.simulation.Entity;
+import com.sun.istack.internal.Nullable;
+
+public class SpriteEntityRenderable implements IEntityRenderable {
+
+	protected Sprite belowFog;
+	private Entity entity;
+	private Sprite aboveFog;
+	
+	public SpriteEntityRenderable(Entity entity, Sprite belowFog, @Nullable Sprite aboveFog) {
+		this.entity = entity;
+		this.belowFog = belowFog;
+		this.aboveFog = aboveFog;
+	}
+	
+	@Override
+	public void update(float elapsed) {
+		
+		Body body = entity.getBody();
+		float angle = MathUtils.radiansToDegrees * body.getAngle();
+		belowFog.setCenter(body.getPosition().x, body.getPosition().y);
+		belowFog.setRotation(angle);
+		
+		if(aboveFog != null) {
+			aboveFog.setCenter(body.getPosition().x, body.getPosition().y);
+			aboveFog.setRotation(angle);
+		}
+	}
+
+	@Override
+	public void render(RenderContext renderContext) {
+		belowFog.draw(renderContext.getBatch());
+	}
+	
+	@Override
+	public void renderAboveFog(RenderContext renderContext) {
+		if(aboveFog != null) {
+			aboveFog.draw(renderContext.getBatch());
+		}
+	}
+
+	@Override
+	public Entity getEntity() {
+		return entity;
+	}
+	
+	
+
+}
